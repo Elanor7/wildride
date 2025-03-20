9d4ec73224c874b8374380c2a529073fd206023b
Elanor7 <e1351096@u.nus.edu>
2025-03-20 15:46:41 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_Uj6rp2UCl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'npm create vite@latest notesapp -- --template react', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '46nd87p4efnjh0b5gknocll3kc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
